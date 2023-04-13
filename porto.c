9d721fb203d0183461d193cdc098b801d6a8e35d
Enrico Parigi <enrico.parigi459@edu.unito.it>
2023-04-13 14:00:56 +0200

    fixato il porto

--- porto.c
+++ porto.c
@@ -52,11 +52,13 @@ int main(int argc, char *argv[]){
 	}
     TEST_ERROR
 
-    //dup2(open("log_porti.txt", O_APPEND | O_CREAT | O_WRONLY, 0666), STDOUT_FILENO);
-    if(freopen("log_porti.txt", "a", stdout)==NULL)
+    dup2(open("log_porti.txt", O_APPEND | O_CREAT | O_WRONLY, 0666), STDERR_FILENO);
+    /*
+    if(freopen("log_porti.txt", "a", stderr)==NULL)
         {perror("freopen ha ritornato NULL");}
+    */
 
-    printf("prova stampa porto %d\n", indice);
+    fprintf(stderr, "prova stampa porto %d\n", indice);
 
     trova_tutti_id(&id_shm_mercato, &id_shm_dettagli_lotti, &id_shm_posizioni_porti, &id_shm_dump, &id_coda_richieste, PARAMETRO);
     SHM_ID[0] = id_shm_mercato;
@@ -70,7 +72,6 @@ int main(int argc, char *argv[]){
     spawnMerciPorti(vptr_shm_mercato, CAST_DETTAGLI_LOTTI(vptr_shm_dettagli_lotti), vptr_shm_dump, id_semaforo_dump, PARAMETRO, indice);
     manda_richieste(vptr_shm_mercato, indice, id_coda_richieste, PARAMETRO);
     /* si sgancia dalle memorie condivise. */
-    sgancia_risorse(vptr_shm_dettagli_lotti, vptr_shm_dump, vptr_shm_mercato, vptr_shm_posizioni_porti);
     /* si dichiara pronto e aspetta. (wait for zero) */
     sem_reserve(id_semaforo_gestione, 0);
     sem_wait_zero(id_semaforo_gestione, 0);
@@ -78,15 +79,16 @@ int main(int argc, char *argv[]){
     do {
         pause();
     } while(1);
+    sgancia_risorse(vptr_shm_dettagli_lotti, vptr_shm_dump, vptr_shm_mercato, vptr_shm_posizioni_porti);
 }
 
 void signal_handler(int signo){
     switch(signo){
         case SIGUSR1:
-            fprintf(stdout,"*** PORTO %d: ricevuto SIGUSR1: data = %d ***\n", indice, CAST_DUMP(vptr_shm_dump)->data);
+            fprintf(stderr,"*** PORTO %d: ricevuto SIGUSR1: data = %d ***\n", indice, CAST_DUMP(vptr_shm_dump)->data);
             break;
         case SIGUSR2:
-            fprintf(stdout,"\nPORTO %d: ricevuto SIGUSR2.\n", indice);
+            fprintf(stderr,"\nPORTO %d: ricevuto SIGUSR2.\n", indice);
             sgancia_risorse(vptr_shm_dettagli_lotti, vptr_shm_dump, vptr_shm_mercato, vptr_shm_posizioni_porti);
             exit(EXIT_SUCCESS);
             break;
